904cf3000dc7c50ebb12e8fdb0a8aacde4fc6c53
Stonemuncher <jonah.burnswell@gmail.com>
2021-09-02 17:36:23 +0100

    Changing to typing reel

--- script.js
+++ script.js
@@ -1,65 +1,57 @@
-const wordsDisplayElement = document.getElementById('wordsDisplay');
-const wordsInputElement = document.getElementById('wordsInput');
+const wordsReel = document.getElementById('wordsReel');
 
 var short = [];
 var medium = [];
 var long = [];
+
+var characters = [];
+var curr_index = 0;
+var curr_char = "";
+
 const loadedLists = new Event('loadedWordLists');
-document.addEventListener('loadedWordLists', function (e) {genBlock()}, false);
+var ready = false;
 
+// Having loaded the word lists, call function to handle further proceedings
+document.addEventListener('loadedWordLists', finishedLoading, false);
 
-
-wordsInputElement.readOnly = "true";
-wordsInputElement.addEventListener('input', () => {
-
-    const correct_letters = wordsDisplayElement.querySelectorAll('span');
-    const typed_letters = wordsInputElement.value.split('');
-
-    let correct = true;
-    var current = "not_set";
-    correct_letters.forEach((char_span, index) => {
-        const typed_char = typed_letters[index];
-        let offsets = char_span.getBoundingClientRect();
-        console.log(offsets.top);
-        if (typed_char == null) {
-            if (current === "not_set") {
-                current = index-1;
-            }
-            char_span.classList.remove('incorrect');
-            char_span.classList.remove('correct');
-            correct = false;
-        }
-        else if (typed_char === char_span.innerText) {
-            char_span.classList.remove('incorrect');
-            char_span.classList.add('correct');
-        } else {
-            correct = false;
-            char_span.classList.remove('correct');
-            char_span.classList.add('incorrect');
-        }
-    })
-    console.log(`Current latest letter typed is number ${current}, which is a '${correct_letters[current].innerText}'`)
-    if (correct) genBlock();
+// Keypress listener
+document.addEventListener("keydown", ({key}) => {
+    if (!ready) {
+        return;
+    }
+    if (key === curr_char.innerText) {
+        curr_char.classList.add('correct')
+        curr_index++;
+        curr_char = characters[curr_index]
+    }
 })
 
-function getWordList(length) {
+async function getWordList(length) {
     return fetch(`${length}.txt`).then(response => response.text());
 }
 
 async function loadLists() {
+    console.log("Loading...")
     short = (await getWordList('short')).split('\n');
     medium = (await getWordList('medium')).split('\n');
     long = (await getWordList('long')).split('\n');
     document.dispatchEvent(loadedLists);   
 }
 
-
-async function genBlock() {
-    wordsInputElement.value = null;
+async function finishedLoading() {
+    console.log("Loaded");
+    characters = await genInitReel();
+    curr_char = characters[0];
+    console.log("Ready")
+    ready = true;
+    
+}
+async function genInitReel() {
+    wordsReel.value = null;
     //console.log(words[Math.floor(Math.random() * words.length]);
 
     var init_block = "";
-    for(let i = 0; i < 30; i++) {
+    for(let i = 0; i < 100; i++) {
         let num = Math.floor(Math.random() * 11);
         //console.log(num);
         if (num <= 0.5) {
@@ -76,13 +68,13 @@ async function genBlock() {
         }
     }
     init_block = init_block.substring(0, init_block.length - 1);
-    init_block.split('').forEach(character => {
+    init_block = init_block.split('').map((char) => {
         const char_span = document.createElement('span');
-        char_span.innerText = character;
-        wordsDisplayElement.appendChild(char_span);
-    })
-
-    wordsInputElement.removeAttribute('readonly');
+        char_span.innerText = char;
+        wordsReel.appendChild(char_span);
+        return char_span;
+    });
+    return init_block;
 }
 
 loadLists();
