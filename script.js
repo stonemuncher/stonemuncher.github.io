6cff124543566b7e52b556c28dc279a4b3f31134
Stonemuncher <jonah.burnswell@gmail.com>
2021-09-02 19:47:38 +0100

    Added removing line and line checking

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const wordsReel = document.getElementById('wordsReel');
 
+const LETTERS = "abcdefghijklmnopqrstuvwxyz"
 var short = [];
 var medium = [];
 var long = [];
@@ -7,7 +8,6 @@ var long = [];
 var characters = [];
 var curr_index = 0;
 var curr_char = "";
-
 const loadedLists = new Event('loadedWordLists');
 var ready = false;
 
@@ -19,12 +19,43 @@ document.addEventListener("keydown", ({key}) => {
     if (!ready) {
         return;
     }
+    let curr_line = curr_char.offsetTop;
     if (key === curr_char.innerText) {
-        curr_char.classList.add('correct')
-        curr_index++;
-        curr_char = characters[curr_index]
+        curr_char.classList.add('correct');
+        curr_char.classList.remove('cursor');
+
+        if (characters[curr_index+1].offsetTop !== curr_line) {
+            curr_line = characters[curr_index+1].offsetTop;
+            console.log("Line change");
+            let line_length = curr_index+1;
+            printCurrLine(line_length);
+            for (let i = 0; i < line_length; i++) {
+                wordsReel.removeChild(characters[i]);
+            }
+            characters = characters.slice(line_length, characters.length-1);
+            printCurrLine(line_length);
+            curr_index = -1;
+            
+        }
+        curr_char = characters[++curr_index]
+        curr_char.classList.add('cursor');
+        
+    }
+
+    // else if (!key && curr_index >= )
+    else if (LETTERS.includes(key)){
+        // Process error and update values for letters
+        console.log(`${key} => ${curr_char.innerText}`)
     }
 })
+
+async function printCurrLine(line_length) {
+    let string_out = "";
+    for (let i = 0; i < line_length; i++) {
+        string_out += characters[i].innerText;
+    }
+    console.log(string_out);
+}
 
 async function getWordList(length) {
     return fetch(`${length}.txt`).then(response => response.text());
@@ -42,6 +73,7 @@ async function finishedLoading() {
     console.log("Loaded");
     characters = await genInitReel();
     curr_char = characters[0];
+    curr_char.classList.add('cursor');
     console.log("Ready")
     ready = true;
     
