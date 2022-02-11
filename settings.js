a0683f777b9ad9da98bf94675de0d0c25c0a1937
Stonemuncher <64792780+stonemuncher@users.noreply.github.com>
2022-02-11 10:02:24 +0000

    Change CHAR_PER_LINE

--- settings.js
+++ settings.js
@@ -1,4 +1,5 @@
 const chance_required = document.getElementById("diffslider");
+
 function reset() {
         if (confirm("Are you sure you want to reset all of your DTT data?") == true) {
                 localStorage.clear();
@@ -6,11 +7,8 @@ function reset() {
         chance_required.value = 50;
 }
 document.getElementById('reset').onclick = reset;
-chance_required.oninput = function() {
-        localStorage.setItem("chancerequired", chance_required.value)
-      }
 
-// Check on load if the user has set their own value in the past, and update the slider to reflect this
-if (localStorage.getItem("chancerequired")) {
-        chance_required.value = localStorage.getItem("chancerequired");
-}
+
+chance_required.oninput = function() {
+        localStorage.set("chancerequired", chance_required.value)
+      }
