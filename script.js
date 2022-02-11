899ac2bbe765c5d5a4d5c56c896d80d86905eee3
Stonemuncher <64792780+stonemuncher@users.noreply.github.com>
2022-02-11 10:06:25 +0000

    Tweak chars_per_line from correct vers

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ const info = document.getElementById('typed');
 
 const LETTERS = "abcdefghijklmnopqrstuvwxyz";
 
+// Number of words that the user can type before the algorithm starts to adaptively add words to the word-reel
+const GRACE_WORDS = 100;
+
+// Chance of a newly added word having to contain difficult letters
+var chance_required = 0.5;
+
+// Key difficulties
 var user_values = {};
+
+// History of Overall values (Summed/total key difficulties)
 var totals_history = [];
+
 var words_typed = 0;
 var worst_five = [];
+
+// Used in the word-reel
 var characters = [];
 var curr_index = 0;
 var curr_char = "";
+
+// Event broadcast once the word list files have been loaded
 const loadedLists = new Event('loadedWordLists');
+
 
 var ready = false;
 var paused = true;
@@ -27,49 +42,71 @@ var word_list = {};
 // Having loaded the word lists, call function to continue
 document.addEventListener('loadedWordLists', finished_loading, false);
 
-window.addEventListener('resize', on_resize);
-
 window.onunload = save_values;
 
 // Keypress listener
 document.addEventListener("keydown", async ({ key }) => {
-        console.clear();
+
         if (!ready) {
                 return;
         }
+        
+        // Logic to pause the app
         if (key == "Enter") {
                 paused = true;
                 info.innerText = "Paused";
         }
         let curr_line = curr_char.offsetTop;
+
+        // If the key inputted was the correct key in sequence
         if (key === curr_char.innerText) {
+
+                // If paused, this means the user has started typing again
+                // last_time is updated to start timing for the time_delta for the next key
                 if (paused) {
                         paused = false;
                         last_time = performance.now();
                         info.innerText = words_typed;
                 }
+
+                // If the key inputted was not a space and was a key that has its difficulty tracked
+                // within the user_values dictionary
                 else if (key in user_values && key != " ") {
                         const time_delta = performance.now() - last_time;
                         last_time = performance.now();
+
+                        // New value generated to be used to modify key difficulty
                         let val = await sigmoid(time_delta / 800);
                         info.innerText = words_typed;
+
+                        // Modify key difficulty
                         user_values[key.toLowerCase()] *= 0.4 + val;
+
+                // This means the input was a space and in the correct place, hence a word was typed
                 } else {
                         words_typed += 1;
+
+                        // Words_typed in the localStorage is updated in case of shutdown, to keep a 
+                        // correct count.
                         localStorage.setItem("typed", words_typed);
-                        if (words_typed % 10 == 0) {
+
+                        // Every 10 words, assuming the words_typed is above the grace period, the Overall score
+                        // (summed key difficulties) is calculated and added to the array (totals_history) and saved to localStorage
+                        if (words_typed % 10 == 0 && words_typed >= GRACE_WORDS) {
                                 let total = get_total();
                                 totals_history.push(total.toFixed(3));
                                 localStorage.setItem("history", JSON.stringify(totals_history));
                         }
                 }
 
+                // Change visuals to move cursor on and make character viewed as correct (style.css)
+                curr_char.classList.remove("incorrect");
                 curr_char.classList.add('correct');
                 curr_char.classList.remove('cursor');
 
+                // Detects for a line change
                 if (characters[curr_index + 1].offsetTop !== curr_line) {
                         curr_line = characters[curr_index + 1].offsetTop;
-
                         console.log("Line change");
 
                         if (curr_index > per_line) {
@@ -99,40 +136,39 @@ document.addEventListener("keydown", async ({ key }) => {
                 for (const key in user_values) {
                         values.push([key, user_values[key]]);
                 }
-
+         
+                // Using the user_values, recalculate the worst_five letters 
+                // i.e. the 5 letters with the highest difficulty values
                 values = reverse_merge(values);
-
                 worst_five = values.slice(0, 5);
+            
         }
-
-        // else if (!key && curr_index >= )
+        // If the key was incorrect, the key difficulty is increased by a flat 0.2
         else if (LETTERS.includes(key) && curr_char.innerText != " ") {
                 user_values[curr_char.innerText] += 0.2;
                 curr_char.classList.add("incorrect");
         }
 })
 
+// Sigmoid function (view documentation)
 async function sigmoid(x) {
         return 1 / (1 + 2.71828 ** (-x))
 }
 
-async function on_resize() {
-        console.log('Resized');
-
-        /*
-        ready = false;
-        per_line = await chars_per_line();
-        ready = true;
-        */
-}
-
+// Called upon closing of the page, saves the number of words typed along with the new user_values
 async function save_values() {
         localStorage.setItem("typed", words_typed);
         for (const letter of LETTERS) {
                 localStorage.setItem(letter, user_values[letter]);
         }
 }
+
+// Called upon loading of the page, checks if entries exist within localStorage
+// If they do, the variable default values are overwritten using the stored value
 async function load_values() {
+        if (localStorage.getItem("chancerequired")) {
+                chance_required = Number(localStorage.getItem("chancerequired"))/100;
+        }
         if (localStorage.getItem("history")) {
                 totals_history = JSON.parse(localStorage.getItem("history"));
         } else {
@@ -149,8 +185,20 @@ async function load_values() {
                         user_values[letter] = 0.5;
                 }
         }
+        values = []
+        for (const key in user_values) {
+                values.push([key, user_values[key]]);
+        }
+
+        values = reverse_merge(values);
+        worst_five = values.slice(0, 5);
 }
 
+/* Used to calculate the number of characters per line on a users screen
+   Since the font size is fixed, the number of characters per line is important and must be accurate
+   so that the word-reel stays at a fixed 3 lines. It works by filling lines and working out when they 
+   wrap - this is only possible since the font is the same width for every letter (monotype)
+*/
 async function chars_per_line() {
         wordsReel.classList.add('hidden');
         let i = 1;
@@ -171,18 +219,12 @@ async function chars_per_line() {
         return (i);
 }
 
-async function print_curr_line(line_length) {
-        let string_out = "";
-        for (let i = 0; i < line_length; i++) {
-                string_out += characters[i].innerText;
-        }
-        //console.log(string_out);
-}
-
+// Helper loading function
 async function get_word_list(length) {
         return fetch(`${length}.txt`).then(response => response.text());
 }
 
+// Used to load the word lists and partition them for rapid searching
 async function load_lists() {
         console.log("Loading...")
         var short = (await get_word_list('short')).split('\n');
@@ -211,6 +253,8 @@ async function load_lists() {
         document.dispatchEvent(loadedLists);
 }
 
+// Once necessaries are loaded, certain calls (self-explanatory) are required
+// before ready=true and the user can start typing.
 async function finished_loading() {
         console.log("Loaded");
         per_line = await chars_per_line();
@@ -221,12 +265,36 @@ async function finished_loading() {
         ready = true;
 }
 
-async function get_word(required) {
+// Get word function takes a paramter referring to the number of letters, from the 
+// 5 highest difficulty letters, the word to be chosen should ideally have. This is used
+// when a call to get_word results in a failure to find a word with the requirements, and is 
+// decremented each recursive call until the requirement is a single letter and there is a 100%
+// chance of the word being found.
+async function get_word(attempt_unknowns) {
+
+        if (!attempt_unknowns) {
+                let x = Math.random();
+                if (x > chance_required) {
+                        attempt_unknowns = 0;
+                } else {
+                        attempt_unknowns = 5;
+                }
+        }
+        let shuffled = [];
+        let required = [];
+        if (attempt_unknowns) {
+                shuffled = worst_five.sort(() => 0.5 - Math.random());
+        }
+        if (words_typed > GRACE_WORDS && ready) {
+                for (let i = 0; i < attempt_unknowns; i++) {
+                        required.push(shuffled[i][0]);
+                }
+        }
+
         const start = performance.now();
         let word = "";
         let word_length = "";
         const num = Math.floor(Math.random() * 11);
-        //console.log(num);
         if (num <= 0.5) {
                 word_length = "long";
         }
@@ -236,15 +304,17 @@ async function get_word(required) {
         else {
                 word_length = "short";
         }
-        if (!required) {
+        // If there are no requirements, choose a random word of the given length
+        if (!required || required.length == 0) {
                 word = word_list[word_length][Math.floor(Math.random() * word_list[word_length].length)]
         }
         else {
-                required = required[0];
                 if (required.length === 1) {
                         const possible = word_list[required[0]][word_length];
                         word = possible[Math.floor(Math.random() * possible.length)];
                 } else {
+                      
+                        // Optimisation using regex discussed in write-up
                         let shortest = { "length": undefined, "letter": "" };
                         required.forEach(letter => {
                                 if ((word_list[letter][word_length].length < shortest["length"]) || !shortest["length"]) {
@@ -267,16 +337,18 @@ async function get_word(required) {
                         }
                         else {
                                 // Handle the fact there are no words with the given requirements and word length
-                                alert("none");
+                                // Change the number of required letters from the 5 and reduce it by one
+                                return get_word(required.length);
+                      
                         }
                 }
         }
         const end = performance.now();
         const time = end - start;
-        //console.log(word + ' ' + time);
         return word;
 }
 
+// Helper function to get an Overall score (sum of key difficulties)
 function get_total() {
         total = 0
         for (const key in user_values) {
@@ -285,27 +357,33 @@ function get_total() {
         return total;
 }
 
+// Generate a new line. This takes a word that is the start of the line, which is 
+// generated in the previous call to the function. This is a requirement so that 
+// the line count stays fixed at 3, and there is no wrapping.
 async function get_line(start) {
-        let required = worst_five[Math.floor(Math.random() * 5)];
+        let word;
+        word = await get_word();
         let line = "";
-        let word = await get_word(required);;
         let done = false;
         if (start) {
                 line = start + " ";
         }
         while (!done) {
-                required = worst_five[Math.floor(Math.random() * 5)];
+                required = [];
                 if (line.length + word.length > per_line && line) {
                         done = true;
                 }
                 else {
                         line += word + " ";
-                        word = await get_word(required);
+                        word = await get_word();
                 }
         }
+
+        // The line, and the 'waiting' word to be included as the start of the next line.
         return [line, word];
 }
 
+// Generate the initial block of lines
 async function gen_first_lines() {
         wordsReel.value = null;
         var init_block = "";
@@ -328,4 +406,6 @@ async function gen_first_lines() {
         }
         return init_block;
 }
+
+// Start loading, after this the respective functions are ready to start running the trainer.
 load_lists();
--- settings.js
+++ settings.js
@@ -1,6 +1,16 @@
+const chance_required = document.getElementById("diffslider");
 function reset() {
         if (confirm("Are you sure you want to reset all of your DTT data?") == true) {
                 localStorage.clear();
         }
+        chance_required.value = 50;
 }
 document.getElementById('reset').onclick = reset;
+chance_required.oninput = function() {
+        localStorage.setItem("chancerequired", chance_required.value)
+      }
+
+// Check on load if the user has set their own value in the past, and update the slider to reflect this
+if (localStorage.getItem("chancerequired")) {
+        chance_required.value = localStorage.getItem("chancerequired");
+}
